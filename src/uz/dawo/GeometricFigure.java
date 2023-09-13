5d042fe7efe53a3d7aacadfce75188cb5d7067fa
odilhon85 <i.o.r@inbox.ru>
2023-09-13 09:15:30 +0500

    Initial commit

---
+++ src/uz/dawo/GeometricFigure.java
@@ -0,0 +1,13 @@
+package uz.dawo;
+
+public class GeometricFigure {
+
+    public double area(){
+        return 0;
+    }
+
+    public double perimetr(){
+        return 0;
+    }
+
+}
---
+++ src/uz/dawo/Triangle.java
@@ -0,0 +1,49 @@
+package uz.dawo;
+
+public class Triangle extends GeometricFigure {
+
+    private int sideA;
+    private int sideB;
+    private int sideC;
+
+    public Triangle(int sideA, int sideB, int sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    @Override
+    public double area() {
+        double s = (sideA + sideB + sideC) / 2.0;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
+    @Override
+    public double perimetr() {
+        return sideA + sideB + sideC;
+    }
+
+    public int getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(int sideA) {
+        this.sideA = sideA;
+    }
+
+    public int getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(int sideB) {
+        this.sideB = sideB;
+    }
+
+    public int getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(int sideC) {
+        this.sideC = sideC;
+    }
+}
